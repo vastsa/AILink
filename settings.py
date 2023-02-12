fe030f17c9f62ee7ba8203746e9bdd24123234f1
vastsa <48862574+vastsa@users.noreply.github.com>
2023-02-12 15:14:52 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,16 @@
+import uuid
+
+
 class Settings:
     API_KEY = ''
     FREE_TOKENS = 100
+    PASSWORD = uuid.uuid4().hex
+
+    def __init__(self):
+        print('*' * 50)
+        print('Settings init')
+        print('Your Password:', self.PASSWORD)
+        print('*' * 50)
 
     def headers(self, key=None):
         return {
